6f9ebf28422a562a6926e76c58eea0715468f371
DonTasko <dontasko.geral@gmail.com>
2026-04-27 22:27:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  */
 
 const CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzIY5XtAybzf0E2UPyJj06V2zzDM_9URCl-A1eSkmcXkBF6lR-7dD6yRw_C2_EiLVhC/exec',
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyhbzKM9rK9VeuOrOIiO_2zFA6ZZP0Kwb345o-CAecduLycSBbJFX6NEoWGnDWn-2bO/exec',
   APP_NAME: 'Don Tasko',
   VERSION: '7.0',
   DEBUG: true,
